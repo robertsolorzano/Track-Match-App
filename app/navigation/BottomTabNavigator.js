c8e7cba604a60f2bbb277d76c6d9f1d07d5ed2a6
Robert Solorzano <robertsolorzano510@gmail.com>
2024-02-17 00:20:00 -0800

    updated icon and name for search in nav bar

--- app/navigation/BottomTabNavigator.js
+++ app/navigation/BottomTabNavigator.js
@@ -3,15 +3,34 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/Homescreen';
 import MainScreen from '../screens/MainScreen';
+import { Ionicons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Search') {
+            iconName = focused ? 'search' : 'search-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'tomato', // Change the active tab color
+        inactiveTintColor: 'gray', // Change the inactive tab color
+      }}
+    >
       <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="MainScreen" component={MainScreen} />
-      {/* Add my songs tab later */}
+      <Tab.Screen name="Search" component={MainScreen} />
+      {/* Add more tabs here */}
     </Tab.Navigator>
   );
 };
