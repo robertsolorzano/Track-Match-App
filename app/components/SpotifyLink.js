cb698f11e42f8e45ba54501e2be6c507fe2eaf32
Robert Solorzano <robertsolorzano510@gmail.com>
2024-02-16 22:09:50 -0800

    removed text decoration from spotofy linka and styled similiar to web app, spotify green hex code and rounded edges for the touchable opacity

--- app/components/SpotifyLink.js
+++ app/components/SpotifyLink.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { TouchableOpacity, Text, StyleSheet, Linking } from 'react-native';
 
 const SpotifyLink = ({ url }) => (
-  <TouchableOpacity onPress={() => Linking.openURL(url)}>
+  <TouchableOpacity style={styles.button} onPress={() => Linking.openURL(url)}>
     <Text style={styles.spotifyLinkText}>Listen on Spotify</Text>
   </TouchableOpacity>
 );
@@ -10,8 +10,18 @@ const SpotifyLink = ({ url }) => (
 export default SpotifyLink;
 
 const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#1DB954',
+    borderRadius: 20,
+    marginTop: 20,
+    overflow: 'hidden',
+    width: 200, 
+    //alignSelf: 'center', //center later when more styles add
+  },
   spotifyLinkText: {
-    color: 'blue',
-    textDecorationLine: 'underline',
+    color: 'white',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    textAlign: 'center',
   },
 });
